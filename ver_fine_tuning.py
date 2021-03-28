8f0793e2093e3215d40b1f49c75263c8c3a8891e
SebOchs <48994875+SebOchs@users.noreply.github.com>
2021-03-28 16:19:58 +0200

    Create ver_fine_tuning.py

---
+++ ver_fine_tuning.py
@@ -0,0 +1,25 @@
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from litT5 import LitAsagFineT5
+
+checkpoint_callback = ModelCheckpoint(
+    monitor='my_metric',
+    mode="max",
+    filepath='models/asag_kn1_t5_{epoch}-{my_metric:.4f}',
+    save_top_k=3
+)
+ver_t5 = LitAsagFineT5(4)
+trainer = pl.Trainer(
+    gpus=2,
+    num_nodes=1,
+    accelerator='ddp',
+    max_epochs=64,
+    accumulate_grad_batches=2,
+    checkpoint_callback=checkpoint_callback,
+    num_sanity_val_steps=0,
+    progress_bar_refresh_rate=10
+)
+
+trainer.fit(ver_t5)
+
+print("finished fine-tuning")
